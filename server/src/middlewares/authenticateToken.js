767034afcf534043f46c9cf65b942a54d7973e2a
MD Bayzed Meer <bayzed.meer@brainstation-23.com>
2024-06-05 17:13:28 +0600

    fix : mising token bug

--- server/src/middlewares/authenticateToken.js
+++ server/src/middlewares/authenticateToken.js
@@ -2,11 +2,12 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
   let token = req.headers.authorization;
-  token = token.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: Missing token" });
   }
+
+  token = token.split(" ")[1];
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
     if (err) {
